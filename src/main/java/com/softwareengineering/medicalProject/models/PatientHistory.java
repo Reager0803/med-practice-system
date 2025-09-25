39e5cb8cda21e5061532ae76cb36967fc47529bc
DavidKilgannon <dave.j.kilgannon@gmail.com>
2025-09-25 18:37:15 -0400

    Removed annotations for testing purposes. Will reinstate later

--- src/main/java/com/softwareengineering/medicalProject/models/PatientHistory.java
+++ src/main/java/com/softwareengineering/medicalProject/models/PatientHistory.java
@@ -15,12 +15,12 @@ public class PatientHistory {
     private Long id;
 
     @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "patientID")
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "patientID")
     private Long patientIDKey;
 
     @JsonIgnore
-    @ManyToMany(fetch = FetchType.LAZY)
+//    @ManyToMany(fetch = FetchType.LAZY)
     private Long procedureIDKey;
 
     private LocalDateTime dateOfProcedure;
