9b643827436a74b49218c64939e779c5e024ab06
Reager <reager0803@gmail.com>
2025-12-02 14:03:48 -0500

    Add files via upload

--- src/main/java/com/softwareengineering/medicalProject/services/PatientService.java
+++ src/main/java/com/softwareengineering/medicalProject/services/PatientService.java
@@ -1,8 +1,9 @@
 package com.softwareengineering.medicalProject.services;
 
+import org.springframework.stereotype.Service;
+
 import com.softwareengineering.medicalProject.models.Patient;
 import com.softwareengineering.medicalProject.repositories.PatientRepository;
-import org.springframework.stereotype.Service;
 
 @Service
 public class PatientService {
@@ -27,18 +28,37 @@ public class PatientService {
         patientRepository.save(patient);
     }
 
-    public void upsertPatient(Long id, String lastName, String middleName, String firstName, String address, String city,
-                           String state, String zip, String phone, Long age, Long height, Long weight, String insurance, String doctor) {
-        Patient patient = null;
-        if (id != 0L) {
+    public void upsertPatient(Long id, String lastName, String middleName, String firstName, 
+                            String address, String city, String state, String zip, 
+                            String phone, Long age, Long height, Long weight, 
+                            String insurance, String doctor) {
+
+        Patient patient;
+
+        if (id != null && id != 0L) {
             patient = patientRepository.findById(id).orElse(null);
+
+            if (patient == null) return;
+            patient.setLastName(lastName);
+            patient.setMiddleName(middleName);
+            patient.setFirstName(firstName);
+            patient.setAddress(address);
+            patient.setCity(city);
+            patient.setState(state);
+            patient.setZip(zip);
+            patient.setPhone(phone);
+            patient.setAge(age);
+            patient.setHeight(height);
+            patient.setWeight(weight);
+            patient.setInsurance(insurance);
+            patient.setPrimaryCareDoctor(doctor);
+
         } else {
-            patient = new Patient(lastName, middleName, firstName, address, city, state, zip, phone, age, height, weight, insurance, doctor);
+            patient = new Patient(lastName, middleName, firstName, address, city, 
+                                state, zip, phone, age, height, weight, insurance, doctor);
         }
 
-        if (patient != null) {
-            patientRepository.save(patient);
-        }
+        patientRepository.save(patient);
     }
 
     public void deletePatient(Long id, String lastName, String middleName, String firstName) {
