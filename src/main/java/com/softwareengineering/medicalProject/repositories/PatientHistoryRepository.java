fda89ac436333733174bb319fe6d071d0238a6c4
Reager <reager0803@gmail.com>
2025-12-03 16:19:23 -0500

    Add files via upload

--- src/main/java/com/softwareengineering/medicalProject/repositories/PatientHistoryRepository.java
+++ src/main/java/com/softwareengineering/medicalProject/repositories/PatientHistoryRepository.java
@@ -1,15 +1,16 @@
 package com.softwareengineering.medicalProject.repositories;
 
-import com.softwareengineering.medicalProject.models.PatientHistory;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
+import com.softwareengineering.medicalProject.models.PatientHistory;
 
 @RepositoryRestResource
 public interface PatientHistoryRepository extends JpaRepository<PatientHistory, Long> {
     List<PatientHistory> findByPatientIDKey(@Param("patientID") Long patientID);
 
-//    List<PatientHistory> findByProcedureId(@Param("procedureId") Long patientID);
+    List<PatientHistory> findByProcedureIDKey(@Param("procedureId") Long procedureID);
 }
